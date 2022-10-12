ceeac4ba152dda7d0c70b9b69064e08fe0c65017
Phuong Tran <57684647+phuongxtran122@users.noreply.github.com>
2022-10-12 01:01:47 -0400

    testing deploy to vercel

--- index.js
+++ index.js
@@ -1,34 +1,59 @@
 const express = require("express");
 const cors = require("cors");
 const pool = require("./db");
+require('dotenv').config()
 const app = express();
 // middleware
 app.use(cors());
 app.use(express.json()); // gives us function to call req.body
-
+app.listen(3000)
+function auth(key) {
+    if (key===process.env.TODO_API_KEY) {
+        return true
+    }
+    return false
+}
 // ROUTES for postgre//
 // create a todo entry
 app.post("/todos", async(req, res) => {
-    // await, async allows await function which only continue when function is complete
-    try{
-        
-        console.log(req.body);
-        const { description } = req.body;
-        // insert description into
-        // returning the table
-        const newTodoEntry = await pool.
-        query(
-            "INSERT INTO todo (description) \
-            VALUES($1) \
-            RETURNING *", 
-            [description]
-        );
-        res.json(newTodoEntry.rows[0])
-    } catch (e) {
-        console.error(e.message);
+    // authentication failed
+    if (!auth(req.headers['todo-api-key'])){
+        res.status(403).json({
+            error: "Unauthorized Permission"
+        })
+    } 
+    else {
+        // await, async allows await function which only continue when function is complete
+        try{
+            
+            console.log(req.body);
+            const { description } = req.body;
+            // insert description into
+            // returning the table
+            const newTodoEntry = await pool.
+            query(
+                "INSERT INTO todo (description) \
+                VALUES($1) \
+                RETURNING *", 
+                [description]
+            );
+            res.json(newTodoEntry.rows[0])
+        } catch (e) {
+            console.error(e.message);
+            res.status(200).json({
+                status: "Failed"
+            });
+            }
+        }
     }
-});
+);
 
+app.get("/favicon.ico", async(req, res) =>{
+
+});
+app.get("/", async(req,res) => {
+    res.json("hi")
+});
 // get all todo items
 app.get("/todos", async(req, res) => {
     try{
@@ -109,3 +134,4 @@ app.delete("/todos/:entryId", async(req, res) => {
 app.listen(5000, () => {
     console.log("server has started on port 5000");
 });
+module.exports = app
